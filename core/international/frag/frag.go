e29d975e13bd45ed61438f6c4e18e1d02ee4ed5f
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-05-27 20:49:47 +0800

    Rename package

--- core/international/frag/frag.go
+++ core/international/frag/frag.go
@@ -1,7 +1,7 @@
 package frag
 
 import (
-	"github.com/apernet/hysteria/core/v2/international/protocol"
+	"github.com/exclavenetwork/hysteria/core/v2/international/protocol"
 )
 
 func FragUDPMessage(m *protocol.UDPMessage, maxSize int) []protocol.UDPMessage {
--- core/server/udp.go
+++ core/server/udp.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/apernet/quic-go"
 
-	"github.com/apernet/hysteria/core/v2/international/frag"
-	"github.com/apernet/hysteria/core/v2/international/protocol"
-	"github.com/apernet/hysteria/core/v2/international/utils"
+	"github.com/exclavenetwork/hysteria/core/v2/international/frag"
+	"github.com/exclavenetwork/hysteria/core/v2/international/protocol"
+	"github.com/exclavenetwork/hysteria/core/v2/international/utils"
 )
 
 const (
